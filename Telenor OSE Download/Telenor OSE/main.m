b3e00027bd05424c61b76ff2052560c74e88bf11
StephenZhu <zhuzp2@lianchuang.com>
2014-08-19 20:08:55 +0800

    12

--- Telenor OSE Download/Telenor OSE/main.m
+++ Telenor OSE Download/Telenor OSE/main.m
@@ -14,6 +14,7 @@ int main(int argc, char * argv[])
 {
     @autoreleasepool {
         //v1.0
+        //sadfa
         //主分支
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([WTAppDelegate class]));
     }
